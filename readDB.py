52b95f19e27f43b80068529d1b0613c328da290d
Nikita Krilov <114663524+Nikkri20@users.noreply.github.com>
2023-04-13 17:25:10 +0500

    Сборка данных в словарь

--- readDB.py
+++ readDB.py
@@ -1,10 +1,12 @@
 import sqlite3
+import json
 
 
 def read_sqlite_table():
+    data_from_database = {}
+    id = 0
     sqlite_connection = sqlite3.connect('db.sqlite3')
     cursor = sqlite_connection.cursor()
-    print("Подключен к SQLite")
 
     sqlite_select_query = """SELECT * from app1_sign"""
     cursor.execute(sqlite_select_query)
@@ -12,13 +14,9 @@ def read_sqlite_table():
     print("Всего строк:", len(records))
 
     for row in records:
-        print("ID:", row[0])
-        print("Name:", row[1])
-        print("Description:", row[2])
-        print("Category:", row[3])
-        print("Photo:", row[4], end="\n\n")
+        id += 1
+        data_from_database[f'{id}'] = {'name': row[1], 'description': row[2], 'category': row[3],
+                                                         'picture': row[4]}
+    print(data_from_database)
 
     cursor.close()
-
-
-read_sqlite_table()
