fa7efaf1b1090a94868ffdc1e1258a75e3ab30d1
Nikita Krilov <114663524+Nikkri20@users.noreply.github.com>
2023-04-15 22:53:27 +0500

    50 -> 500 описание

--- app1/models.py
+++ app1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Sign(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
     category = models.CharField(max_length=50)
 
     photo = models.ImageField(upload_to='images/')
