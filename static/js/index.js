2a6e0aadf0d533ac0479d0ca4abcb61c139b82b5
Сергей <sery66666@mail.ru>
2023-05-08 13:43:16 +0500

    week-11

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,8 @@ const filterButton = document.querySelector('.signs-filter');
 const filterList = document.querySelector('.filter-list');
 const filterListItems = document.querySelectorAll('.filter-list-item');
 const buttonShouMore = document.querySelector('.button-shou-more')
-
+const popup = document.querySelector('.popup');
+const popupButtonClose = document.querySelector('.popup-button-close');
 const SIGNS = {
     sign1:{
         name: 'Name1',
@@ -45,6 +46,11 @@ const SIGNS = {
 
 const signsContainer = document.querySelector('.signs-container');
 const signTemplate = document.querySelector('#sign').content;
+const pictureSign = document.querySelector('.picture-sign');
+const worldSign = document.querySelector('.world-sign');
+const popupNameSign = document.querySelector('.popup-name-sign');
+const popupDescriptionSign = document.querySelector('.popup-description-sign');
+
 
 function appendSign(Signs) {
     const fragment = document.createDocumentFragment();
@@ -54,29 +60,32 @@ function appendSign(Signs) {
         //currentSign.querySelector('.description-sign').textContent = sign[1].description;
         currentSign.querySelector('.name-sign').textContent = sign[1].name;
         currentSign.querySelector('.sign-container').dataset.category = sign[1].category;
-        fragment.appendChild(currentSign)
+        fragment.appendChild(currentSign);
+
     });
     signsContainer.appendChild(fragment);
     const card = document.querySelectorAll('.sign-container');
     let maxCard = 10;
     function showMore(maxCard) {
-      for (let i = 0; i < card.length; i++) {
-        if (i < maxCard) {
-          card[i].style.display = 'flex';
-          if (maxCard === card.length || maxCard > card.length) {
-              buttonShouMore.style.display = 'none';
+        for (let i = 0; i < card.length; i++) {
+            if (i < maxCard) {
+                card[i].classList.remove('hidden-card');
+              if (maxCard === card.length || maxCard > card.length) {
+                  buttonShouMore.style.display = 'none';
+              }
+            } else  {
+                break;
+            }
           }
-        } else  {
-            break;
         }
-      }
-    }
     showMore(maxCard);
     buttonShouMore.onclick = function() {
-      maxCard += 10;
-      showMore(maxCard);
+        maxCard += 10;
+        showMore(maxCard);
     }
 }
+
+
 
 filterButton.addEventListener('click', () => {
     if(filterList.classList.contains('hidden'))
@@ -88,6 +97,19 @@ filterButton.addEventListener('click', () => {
     }
 });
 
+const openPopup = (sign) => {
+    pictureSign.src = sign[1].picture;
+    worldSign.src = '/static/img/Rectangle%205.svg';
+    popupNameSign.textContent = sign[1].name;
+    popupDescriptionSign.textContent = sign[1].description;
+    popup.classList.remove('hidden');
+}
+
+popupButtonClose.onclick = function () {
+    popup.classList.add('hidden');
+}
+
+
 fetch('send')
     .then((response) => {
       if(response.ok) {
@@ -96,14 +118,28 @@ fetch('send')
       throw new Error(`${response.status} ${response.statusText}`);
   }).then((data) => {
     appendSign(data)
-  }).then(() => {
+    const signsArray = [];
+    Object.entries(data).forEach(function(sign) {
+        signsArray.push(sign[1])
+    });
+    return signsArray;
+
+  }).then((data) => {
       const cards = document.querySelectorAll('.sign-container');
+      cards.forEach((card,index) => {
+          card.onclick = () => {
+              pictureSign.src = data[index].picture;
+              worldSign.src = '/static/img/Rectangle%205.svg';
+              popupNameSign.textContent = data[index].name;
+              popupDescriptionSign.textContent = data[index].description;
+              popup.classList.remove('hidden');
+          }
+      })
     filterListItems.forEach((item) => {
        item.addEventListener('click', () => {
           filterButton.querySelector('span').textContent = item.textContent;
           filterList.classList.add('hidden');
           for (let card of cards) {
-                console.log(item);
                 if (card.dataset.category !== item.dataset.category && item.dataset.category !== 'all-signs') {
                     card.classList.add('hidden-card');
                 } else {
