18a68faa6eb2b3d7758e8f4ebe81b545c4078e64
Nikita Krilov <114663524+Nikkri20@users.noreply.github.com>
2023-05-25 16:18:40 +0500

    Исправление readDB

--- readDB.py
+++ readDB.py
@@ -15,22 +15,30 @@ def read_sqlite_table(table):
         for row in records:
             id += 1
             data_from_database[f'{id}'] = {'name': row[1], 'description': row[2], 'category': row[3],
-                                           'picture': "/images/" + row[4], 'pictureWorld': "/images/" + row[5]}
+                                           'picture': "/images/" + row[4]}
         return data_from_database
 
     if table == "app1_test":
-        data_from_database = {}
+        data_from_database_for_questions = {}
+        data_from_database_for_answers = {}
         textQuestions = []
         answersList = []
         id = 0
         for row in records:
             textQuestions = row[1].split(",")
             answersList = row[2].split(",")
+        for i in range(len(textQuestions)):
+            textQuestions[i] = " ".join(textQuestions[i].split())
+            textQuestions[i] = textQuestions[i][0].title() + textQuestions[i][1:]
+        for i in range(len(answersList)):
+            answersList[i] = " ".join(answersList[i].split())
         for row in records:
             id += 1
-            data_from_database[f'{id}'] = {'number': id, 'picture': "/images/" + row[3], 'textQuestions': textQuestions,
+            data_from_database_for_questions[f'{id}'] = {'number': id, 'picture': "/images/" + row[3], 'textQuestions': textQuestions,
                                            'answersList': answersList}
-        return data_from_database
+            data_from_database_for_answers[f'{id}'] = {'number': id, 'puctureSign': "/images/" + row[3], 'puctureWorld': "/images/" + row[6],
+                                                       'nameSign': row[4], 'descriptionSign': row[5]}
+        return data_from_database_for_questions#, data_from_database_for_answers
 
     else:
         print("Недопустимая таблица")
