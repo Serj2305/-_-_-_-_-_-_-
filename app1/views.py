287909f336bc051dfe07d5a29e3bc8d54e508944
Nikkri20 <Nikita.Krylov@urfu.me>
2023-07-04 14:18:46 +0500

    Страница входа

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.messages import get_messages
 from django.shortcuts import render
-# Подключение стандартной формы для регистрации
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from readDB import read_sqlite_table
 
@@ -9,7 +11,6 @@ def index_page(request):
     return render(request, 'index.html')
 
 
-# Create your views here.
 def project_page(request):
     return render(request, 'project-page.html')
 
@@ -35,6 +36,7 @@ def send(request):
 def sendTest(request):
     return JsonResponse(read_sqlite_table("app1_test"))
 
+
 # Функция регистрации
 def registr(request):
     # Массив для передачи данных шаблонны
@@ -50,17 +52,47 @@ def registr(request):
             # Передача формы к рендару
             data['form'] = form
             # Передача надписи, если прошло всё успешно
-            data['res'] = "Всё прошло успешно"
             return render(request, 'registr.html', data)
         elif not form.is_valid():
             data['form'] = form
-            data['res'] = "Ошибка"
             return render(request, 'registr.html', data)
-    else:  # Иначе
+    else:
         # Создаём форму
         form = UserCreationForm()
         # Передаём форму для рендеринга
         data['form'] = form
-        data['res'] = "Mistakes"
         return render(request, 'registr.html', data)
 
+
+# def sign_in(request):
+#     if request.method == 'GET':
+#         form = AuthenticationForm()
+#         return render(request, 'login.html', {'form': form})
+#
+#     elif request.method == 'POST':
+#         form = AuthenticationForm(request.POST)
+#
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=username, password=password)
+#             if user:
+#                 messages.success(request, f'Hi {username.title()}, welcome back!')
+#                 return render(request, 'login.html', {'form': form})
+#
+#         else:
+#             messages.error(request, f'Invalid username or password')
+#             return render(request, 'login.html', {'form': form})
+
+def sign_in(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(None, data=request.POST)
+        if form.is_valid():
+            form.clean()
+            user = form.get_user()
+            if user is not None:
+                login(request, user)
+                messages.info(request, 'Вы успешно зашли в аккаунт')
+                return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {'form': form})
